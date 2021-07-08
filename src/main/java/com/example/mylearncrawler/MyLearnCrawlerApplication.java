f2a8bdd6d7e12f3c20b8d539a69b389b5e13e1d9
mousans <2494778441@qq.com>
2021-07-08 22:29:59 +0800

    创建项目完成

---
+++ src/main/java/com/example/mylearncrawler/MyLearnCrawlerApplication.java
@@ -0,0 +1,13 @@
+package com.example.mylearncrawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyLearnCrawlerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyLearnCrawlerApplication.class, args);
+    }
+
+}
